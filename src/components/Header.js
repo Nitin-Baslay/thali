aff9f7600b425be15d7620c95ddda71e4d176b60
Nitin Kumar <113196521+Nitin-Baslay@users.noreply.github.com>
2023-05-06 11:24:05 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import "./Header.css";
+const Header = () => {
+  return (
+    <div >
+      <img
+        src="https://bl-i.thgim.com/public/specials/flight-plan/7ipbuc/article32950475.ece/alternates/FREE_1200/BL28LOGO"
+        alt="namskar"
+        className="maharaj"
+      />
+      <div className="header">
+        <h1 className="head1">Welcome to Baslay's Restaurant</h1>
+      </div>
+    </div>
+  );
+};
+export default Header;
